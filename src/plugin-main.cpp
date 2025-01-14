8ef1f9289937d72e6672e022a6d02fd9f063220a
Zihan Pan <zp222@ic.ac.uk>
2025-01-14 13:12:54 +0000

    Fix: add heart_rate_source_info to CMakeLists and remove unnecessary include
    Co-authored-by: GeGao922 <GeGao922@users.noreply.github.com>
    Co-authored-by: Maciek03102003 <Maciek03102003@users.noreply.github.com>

--- src/plugin-main.cpp
+++ src/plugin-main.cpp
@@ -19,15 +19,12 @@ with this program. If not, see <https://www.gnu.org/licenses/>
 #include <obs-module.h>
 #include "plugin-support.h"
 #include "heart_rate_widget.hpp"
-#include "heart_rate_source_info.h"
 
-// comment
-HeartRateWidget *heartRateWidget = nullptr;
-extern struct obs_source_info heart_rate_source_info;
-
-//main
 OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
+
+HeartRateWidget *heartRateWidget = nullptr;
+extern struct obs_source_info heart_rate_source_info;
 
 bool obs_module_load(void)
 {
