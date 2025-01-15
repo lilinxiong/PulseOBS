d53ce5bd55e3bf2c3b62e65696f0a0a758192edf
Zihan Pan <zp222@ic.ac.uk>
2025-01-15 15:37:12 +0000

    Refactor: fix cmake and clang format

--- src/heart_rate_source.cpp
+++ src/heart_rate_source.cpp
@@ -16,38 +16,45 @@ const char *get_heart_rate_source_name(void *)
 
 struct heart_rate_source {
 	obs_source_t *source;
-	gs_texrender_t *texrender; // buffer in GPU where rendering operations are performed
-	gs_stagesurf_t *stagesurface; // facilitates transferring rendered textures from the GPU to CPU
+	gs_texrender_t *
+		texrender; // buffer in GPU where rendering operations are performed
+	gs_stagesurf_t *
+		stagesurface; // facilitates transferring rendered textures from the GPU to CPU
 };
 
-static void processBGRAData(uint8_t *data, uint32_t width, uint32_t height, uint32_t linesize) {
-    uint64_t sumB = 0, sumG = 0, sumR = 0, sumA = 0;
-    uint32_t pixel_count = width * height;
+static void processBGRAData(uint8_t *data, uint32_t width, uint32_t height,
+			    uint32_t linesize)
+{
+	uint64_t sumB = 0, sumG = 0, sumR = 0, sumA = 0;
+	uint32_t pixel_count = width * height;
 
-    for (uint32_t y = 0; y < height; ++y) {
-        for (uint32_t x = 0; x < width; ++x) {
-            uint8_t B = data[y * linesize + x * 4 + 0];
-            uint8_t G = data[y * linesize + x * 4 + 1];
-            uint8_t R = data[y * linesize + x * 4 + 2];
-            uint8_t A = data[y * linesize + x * 4 + 3];
-            sumB += B;
-            sumG += G;
-            sumR += R;
-            sumA += A;
-        }
-    }
+	for (uint32_t y = 0; y < height; ++y) {
+		for (uint32_t x = 0; x < width; ++x) {
+			uint8_t B = data[y * linesize + x * 4 + 0];
+			uint8_t G = data[y * linesize + x * 4 + 1];
+			uint8_t R = data[y * linesize + x * 4 + 2];
+			uint8_t A = data[y * linesize + x * 4 + 3];
+			sumB += B;
+			sumG += G;
+			sumR += R;
+			sumA += A;
+		}
+	}
 
-    double averageB = static_cast<double>(sumB) / pixel_count;
-    double averageG = static_cast<double>(sumG) / pixel_count;
-    double averageR = static_cast<double>(sumR) / pixel_count;
-    double averageA = static_cast<double>(sumA) / pixel_count;
+	double averageB = static_cast<double>(sumB) / pixel_count;
+	double averageG = static_cast<double>(sumG) / pixel_count;
+	double averageR = static_cast<double>(sumR) / pixel_count;
+	double averageA = static_cast<double>(sumA) / pixel_count;
 
-    obs_log(LOG_INFO, "Average B: %f, G: %f, R: %f, A: %f\n", averageB, averageG, averageR, averageA);
+	obs_log(LOG_INFO, "Average B: %f, G: %f, R: %f, A: %f\n", averageB,
+		averageG, averageR, averageA);
 }
 
-static bool getBGRAFromStageSurface(struct heart_rate_source *hrs, uint32_t &width, uint32_t &height) {
+static bool getBGRAFromStageSurface(struct heart_rate_source *hrs,
+				    uint32_t &width, uint32_t &height)
+{
 	obs_log(LOG_INFO, "--------BGRASTART!!!!!!!!");
-	
+
 	// Check if the source is enabled
 	if (!obs_source_enabled(hrs->source)) {
 		return false;
@@ -58,16 +65,17 @@ static bool getBGRAFromStageSurface(struct heart_rate_source *hrs, uint32_t &wid
 	// Retrieve the target source of the filter
 	obs_source_t *target = obs_filter_get_target(hrs->source);
 	if (!target) {
-    return false;
+		return false;
 	}
 
-	obs_log(LOG_INFO, "--------RETRIEVING BASE DIMENSIONS OF TARGET SOURCE!!!!!!!!");
+	obs_log(LOG_INFO,
+		"--------RETRIEVING BASE DIMENSIONS OF TARGET SOURCE!!!!!!!!");
 
 	// Retrieve the base dimensions of the target source
 	width = obs_source_get_base_width(target);
 	height = obs_source_get_base_height(target);
 	if (width == 0 || height == 0) {
-    return false;
+		return false;
 	}
 
 	obs_log(LOG_INFO, "--------RESETTING THE TEXTURE RENDERER!!!!!!!!");
@@ -76,16 +84,16 @@ static bool getBGRAFromStageSurface(struct heart_rate_source *hrs, uint32_t &wid
 	gs_texrender_reset(hrs->texrender);
 	obs_log(LOG_INFO, "width: %d", width);
 	obs_log(LOG_INFO, "height: %d", height);
-	if(!hrs->texrender)
-	{
+	if (!hrs->texrender) {
 		obs_log(LOG_INFO, "texrender is null");
 	}
 	if (!gs_texrender_begin(hrs->texrender, width, height)) {
 		obs_log(LOG_INFO, "--------FAILED TO START RENDERING!!!!!!!!");
-    return false;
+		return false;
 	}
 
-	obs_log(LOG_INFO, "--------FINISHED RESETTING THE TEXTURE RENDERER!!!!!!!!");
+	obs_log(LOG_INFO,
+		"--------FINISHED RESETTING THE TEXTURE RENDERER!!!!!!!!");
 
 	// Clear up and set up rendering
 	// - Clears the rendering surface with a zeroed background
@@ -101,7 +109,8 @@ static bool getBGRAFromStageSurface(struct heart_rate_source *hrs, uint32_t &wid
 	vec4_zero(&background); // zeroes a vector
 
 	// Clear the rendering surface from the previous frame processing, with the specified background colour. The GS_CLEAR_COLOR flag indicates that the colour buffer should be cleared. This sets the colour to &background colour
-	gs_clear(GS_CLEAR_COLOR, &background, 0.0f, 0); // Clears color/depth/stencil buffers 
+	gs_clear(GS_CLEAR_COLOR, &background, 0.0f,
+		 0); // Clears color/depth/stencil buffers
 
 	// Sets up an orthographic projection matrix. This matrix defines a 2D rendering space where objects are rendered without perspective distortion
 	// Parameters:
@@ -111,7 +120,8 @@ static bool getBGRAFromStageSurface(struct heart_rate_source *hrs, uint32_t &wid
 	// static_cast<float>(height): The top coordinate of the projection, set to the height of the target source
 	// -100.0f: The near clipping plane. The near clipping plane is the closest plane to the camera. Objects closer to the camera than this plane are clipped (not rendered). It helps to avoid rendering artifacts and improves depth precision by discarding objects that are too close to the camera
 	// 100.0f: The far clipping plane. The far clipping plane is the farthest plane from the camera. Objects farther from the camera than this plane are clipped (not rendered).It helps to limit the rendering distance and manage depth buffer precision by discarding objects that are too far away
-	gs_ortho(0.0f, static_cast<float>(width), 0.0f, static_cast<float>(height), -100.0f, 100.0f);
+	gs_ortho(0.0f, static_cast<float>(width), 0.0f,
+		 static_cast<float>(height), -100.0f, 100.0f);
 
 	// This function saves the current blend state onto a stack. The blend state includes settings that control how colors from different sources are combined during rendering. By pushing the current blend state, you can make temporary changes to the blend settings and later restore the original settings by popping the blend state from the stack
 	gs_blend_state_push();
@@ -134,34 +144,38 @@ static bool getBGRAFromStageSurface(struct heart_rate_source *hrs, uint32_t &wid
 
 	// Retrieve the old existing stage surface
 	if (hrs->stagesurface) {
-		uint32_t stagesurf_width = gs_stagesurface_get_width(hrs->stagesurface);
-		uint32_t stagesurf_height = gs_stagesurface_get_height(hrs->stagesurface);
+		uint32_t stagesurf_width =
+			gs_stagesurface_get_width(hrs->stagesurface);
+		uint32_t stagesurf_height =
+			gs_stagesurface_get_height(hrs->stagesurface);
 		// If it still matches the new width and height, reuse it
 		if (stagesurf_width != width || stagesurf_height != height) {
 			// Destroy the old stage surface
 			gs_stagesurface_destroy(hrs->stagesurface);
 			hrs->stagesurface = nullptr;
 		}
-  }
+	}
 
 	// Create a new stage surface if necessary
-  if (!hrs->stagesurface) {
-    hrs->stagesurface = gs_stagesurface_create(width, height, GS_BGRA);
-  }
+	if (!hrs->stagesurface) {
+		hrs->stagesurface =
+			gs_stagesurface_create(width, height, GS_BGRA);
+	}
 
 	// Use gs_stage_texture to stage the texture from the texture renderer (hrs->texrender) to the stage surface (hrs->stagesurface). This operation transfers the rendered texture to the stage surface for further processing
-	gs_stage_texture(hrs->stagesurface, gs_texrender_get_texture(hrs->texrender));
+	gs_stage_texture(hrs->stagesurface,
+			 gs_texrender_get_texture(hrs->texrender));
 
 	// Use gs_stagesurface_map to map the stage surface and retrieve the video data and line size. The video_data pointer will point to the BGRA data, and linesize will indicate the number of bytes per line
 	uint8_t *video_data; // A pointer to the memory location where the BGRA data will be accessible
 	uint32_t linesize; // The number of bytes per line (or row) of the image data
 	// The gs_stagesurface_map function creates a mapping between the GPU memory and the CPU memory. This allows the CPU to access the pixel data directly from the GPU memory
 	if (!gs_stagesurface_map(hrs->stagesurface, &video_data, &linesize)) {
-			return false;
-	} 
+		return false;
+	}
 
 	{
-		// TODO: CV MAT, need a lock 
+		// TODO: CV MAT, need a lock
 	}
 
 	// // Call our algorithm :)
@@ -224,16 +238,17 @@ void heart_rate_source_render(void *data, gs_effect_t *effect)
 	UNUSED_PARAMETER(effect);
 
 	obs_log(LOG_INFO, "--------------Start of RENDER!!!!!!!!!");
-	struct heart_rate_source *hrs = reinterpret_cast<struct heart_rate_source *>(data);
+	struct heart_rate_source *hrs =
+		reinterpret_cast<struct heart_rate_source *>(data);
 
 	if (!hrs->source) {
 		obs_log(LOG_INFO, "--------NO SOURCE!!!!!!!!");
 		return;
 	}
-	
+
 	uint32_t width, height;
 	getBGRAFromStageSurface(hrs, width, height);
-	
+
 	obs_source_skip_video_filter(hrs->source);
 }
 
